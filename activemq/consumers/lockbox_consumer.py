fbdde08671b501ae85d249c98a4cc26397198b51
Alhagie Bai Cham <cham207388@gmail.com>
2024-11-21 13:15:38 -0600

    timestamp

--- activemq/consumers/lockbox_consumer.py
+++ activemq/consumers/lockbox_consumer.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime, timezone
+from tzlocal import get_localzone
 
-from dao.database import SessionLocal
 from ..producers.elis_producer import ElisProducer
 from .consumer import Consumer
 
@@ -16,7 +16,9 @@ class LockBoxConsumer(Consumer):
         try:
             # Modify the message
             message_data = json.loads(message)
-            message_data["timestamp"] = datetime.now(timezone.utc).strftime("%d:%m:%Y %H:%M:%S")
+            # message_data["timestamp"] = datetime.now(timezone.utc).strftime("%d:%m:%Y %H:%M:%S")
+            local_timezone = get_localzone()
+            message_data["timestamp"] = datetime.now(local_timezone).strftime("%d:%m:%Y %H:%M:%S")
             modified_message = json.dumps(message_data)
 
             # Forward the modified message to /queue/elis
