b09c167f9b72069645198aebc47c506dcafa84bc
Alhagie Bai Cham <cham207388@gmail.com>
2024-11-21 13:03:33 -0600

    active mq

--- activemq/active_mq.py
+++ activemq/active_mq.py
@@ -4,18 +4,7 @@ from sqlalchemy.orm import Session
 
 from dao.model import Post, convert_to_post
 from utils.env_variables import ACTIVEMQ_USER, ACTIVEMQ_PASSWORD, ACTIVEMQ_PORT, ACTIVEMQ_HOST
-from dao.database import SessionLocal
 
-
-class MyListener(stomp.ConnectionListener):
-    def on_error(self, frame):
-        print(f'Error: {frame.body}')
-    def on_message(self, frame):
-        print(f'Received message: {frame.body}')
-        print(f'type: {type(frame.body)}')
-        post = sanitize_post(frame.body)
-        save_post_to_db(post, SessionLocal())
-        print(f'Saved post to db...')
 
 class ActiveMQ:
     def __init__(self):
@@ -23,13 +12,18 @@ class ActiveMQ:
         self.port = ACTIVEMQ_PORT
         self.username = ACTIVEMQ_USER
         self.password = ACTIVEMQ_PASSWORD
-        self.connection = stomp.Connection([(ACTIVEMQ_HOST, ACTIVEMQ_PORT)])
+        self.connection = None
 
     def connect(self):
-        print(f'host: {self.host}, port: {self.port}, username: {self.username}, password: {self.password}')
-        self.connection.set_listener('', MyListener())
-        self.connection.connect(self.username, self.password, wait=True)
+        if not self.connection or not self.connection.is_connected():
+            self.connection = stomp.Connection([(self.host, self.port)])
+            self.connection.connect(self.username, self.password, wait=True)
         return self.connection
+
+    def disconnect(self):
+        if self.connection and self.connection.is_connected():
+            self.connection.disconnect()
+            self.connection = None
 
 def save_post_to_db(post: Post, db: Session):
     try:
