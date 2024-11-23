77e922a2c3cbdc6fdb5d6986b5f3d64fd6aabb85
Alhagie Bai Cham <cham207388@gmail.com>
2024-11-23 08:35:42 -0600

    prepping for fanout pattern

--- activemq/active_mq.py
+++ activemq/active_mq.py
@@ -1,8 +1,4 @@
 import stomp
-import json
-from sqlalchemy.orm import Session
-
-from dao.model import Post, convert_to_post
 from utils.env_variables import ACTIVEMQ_USER, ACTIVEMQ_PASSWORD, ACTIVEMQ_PORT, ACTIVEMQ_HOST
 
 
@@ -24,18 +20,3 @@ class ActiveMQ:
         if self.connection and self.connection.is_connected():
             self.connection.disconnect()
             self.connection = None
-
-# def save_post_to_db(post: Post, db: Session):
-#     try:
-#         save_post = convert_to_post(post)
-#         print("Saving...")
-#         db.add(save_post)
-#         db.commit()
-#     except Exception as e:
-#         print(f"Failed to save hero to database: {e}")
-#
-# def sanitize_post(body):
-#
-#     post_data = json.loads(body)  # Parse the JSON string to a dictionary
-#     post = Post(**post_data) # Cast dictionary to Post
-#     return post
