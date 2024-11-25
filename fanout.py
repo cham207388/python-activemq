bc590ede11c4335102fc691c3ba6f546fe9f4792
Alhagie Bai Cham <cham207388@gmail.com>
2024-11-25 10:12:42 -0600

    prepping for fanout pattern

--- fanout.py
+++ fanout.py
@@ -1,49 +1,49 @@
-import stomp
-
-# Create a connection
-conn = stomp.Connection([('localhost', 61613)])
-conn.start()
-conn.connect('admin', 'admin', wait=True)
-
-# Send a message to the virtual topic
-conn.send(body='Hello World!', destination='/topic/VirtualTopic.mytopic')
-
-conn.disconnect()
-
----consumer 1
-import stomp
-
-class MyListener(stomp.ConnectionListener):
-    def on_message(self, headers, message):
-        print(f"Received message: {message}")
-
-# Create a connection
-conn = stomp.Connection([('localhost', 61613)])
-conn.set_listener('', MyListener())
-conn.start()
-conn.connect('admin', 'admin', wait=True)
-
-# Subscribe to the virtual topic
-conn.subscribe(destination='/queue/Consumer.A.VirtualTopic.mytopic', id=1, ack='auto')
-
-# Keep the connection alive
-conn.disconnect()
-
----consumer 2
-import stomp
-
-class MyListener(stomp.ConnectionListener):
-    def on_message(self, headers, message):
-        print(f"Received message: {message}")
-
-# Create a connection
-conn = stomp.Connection([('localhost', 61613)])
-conn.set_listener('', MyListener())
-conn.start()
-conn.connect('admin', 'admin', wait=True)
-
-# Subscribe to the virtual topic
-conn.subscribe(destination='/queue/Consumer.B.VirtualTopic.mytopic', id=1, ack='auto')
-
-# Keep the connection alive
-conn.disconnect()
+# import stomp
+#
+# # Create a connection
+# conn = stomp.Connection([('localhost', 61613)])
+# conn.start()
+# conn.connect('admin', 'admin', wait=True)
+#
+# # Send a message to the virtual topic
+# conn.send(body='Hello World!', destination='/topic/VirtualTopic.mytopic')
+#
+# conn.disconnect()
+#
+# ---consumer 1
+# import stomp
+#
+# class MyListener(stomp.ConnectionListener):
+#     def on_message(self, headers, message):
+#         print(f"Received message: {message}")
+#
+# # Create a connection
+# conn = stomp.Connection([('localhost', 61613)])
+# conn.set_listener('', MyListener())
+# conn.start()
+# conn.connect('admin', 'admin', wait=True)
+#
+# # Subscribe to the virtual topic
+# conn.subscribe(destination='/queue/Consumer.A.VirtualTopic.mytopic', id=1, ack='auto')
+#
+# # Keep the connection alive
+# conn.disconnect()
+#
+# ---consumer 2
+# import stomp
+#
+# class MyListener(stomp.ConnectionListener):
+#     def on_message(self, headers, message):
+#         print(f"Received message: {message}")
+#
+# # Create a connection
+# conn = stomp.Connection([('localhost', 61613)])
+# conn.set_listener('', MyListener())
+# conn.start()
+# conn.connect('admin', 'admin', wait=True)
+#
+# # Subscribe to the virtual topic
+# conn.subscribe(destination='/queue/Consumer.B.VirtualTopic.mytopic', id=1, ack='auto')
+#
+# # Keep the connection alive
+# conn.disconnect()
