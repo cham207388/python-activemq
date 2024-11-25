1c7e4bbbb5b5f844b7b3d763f5e18e50a1fb3ca3
Alhagie Bai Cham <cham207388@gmail.com>
2024-11-25 14:38:37 -0600

    refactor

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -3,17 +3,10 @@ resource "aws_security_group" "mq_security_group" {
   description = "Security group for Amazon MQ"
 
   ingress {
-    from_port   = 61614               # STOMP over SSL
+    from_port   = 8162          # STOMP over SSL
     to_port     = 61614
     protocol    = "tcp"
-    cidr_blocks = ["203.0.113.0/24"]  # Replace with your trusted IP or CIDR range
-  }
-
-  ingress {
-    from_port   = 8162                # Web Console (optional)
-    to_port     = 8162
-    protocol    = "tcp"
-    cidr_blocks = ["203.0.113.0/24"]  # Replace with your trusted IP or CIDR range
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your trusted IP or CIDR range
   }
 
   egress {
